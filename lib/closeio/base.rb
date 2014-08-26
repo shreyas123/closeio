a637f8386449af4fd57d52b7f14b702269f768a6
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2014-08-26 14:20:30 +0200

    Added 2nd parameter to basic_auth

--- lib/closeio/base.rb
+++ lib/closeio/base.rb
@@ -3,7 +3,7 @@ module Closeio
 
     include HTTParty
     base_uri   'https://app.close.io/api/v1'
-    basic_auth Closeio.api_key
+    basic_auth Closeio.api_key, ''
     headers 'Content-Type' => 'application/json'
     #debug_output $stdout
     format :json
